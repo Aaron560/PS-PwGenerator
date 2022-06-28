10e4044013c6772b1c4528bbe389e4b9df151e30
Aaron560 <aaron.ashcraft7@gmail.com>
2022-06-27 21:05:57 -0400

    Added more functionlaity.

--- PasswordScript.ps1
+++ PasswordScript.ps1
@@ -7,8 +7,31 @@ $Pass = "Insert your value here."
 $Passwordlength = '10'
 $NonAlphanumeric = '6'
 $CreatePassword =[System.Web.Security.Membership]::GeneratePassword($Passwordlength, $NonAlphanumeric)
+$wifiNames =
+'Rebellious Amish Family',
+'Not a Meth Lab',
+'Definitely a Meth Lab',
+'FBI Surveillance Van 3',
+'I am Always Watching',
+'Hack of All Trades',
+'Russian Hackers',
+'LAN-Rover',
+'You Will Regret This',
+'Clean Your Glasses!',
+'Wi-Fi for the Recently Deceased',
+'The Promised LAN',
+'Reserved for Guests I Hate',
+'Open Sesame',
+'Jump on the Bandwidth',
+'Look Ma, No Wires!',
+'Im Under Your Bed',
+'Enter the Dragons Wi-Fi',
+'Wi of the Figer',
+'Bob DyLAN'
 
 $CreatePassword
+$NewwifiName = Get-Random -InputObject $wifiNames
+$NewwifiName
 
 
 
@@ -36,4 +59,11 @@ Start-Sleep -Seconds 3
 
 $SSID_24g = $ie.document.getElementByID('ssid_24g')
 $SSID_24g.click()
-$SSID_24g.value = "test"
+$SSID_24g.value = "$NewwifiName"
+
+$pwd_24g = $ie.document.getElementByID('pwd_24g')
+$pwd_24g.click()
+$pwd_24g.value = "$CreatePassword"
+
+$Save = $ie.document.getElementByID('save')
+$Save.click()
